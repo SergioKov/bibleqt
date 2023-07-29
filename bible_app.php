34b6528be93ba2ed28d4386db85242a4565f3e1e
SergioKov <sergmac@Mac-mini-de-Sergio.local>
2023-07-29 16:41:16 +0200

    2023-07-29 16:37 - terminados los salmos de ukr_hom - 2

--- bible_app.php
+++ bible_app.php
@@ -7,7 +7,7 @@ header('Content-type: text/html; charset=utf-8');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My Bible Quote-1</title>
+    <title>My Bible Quote</title>
     <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet'>
     <link rel="stylesheet" href="bible_app.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.3/dist/sweetalert2.all.min.js"></script>
