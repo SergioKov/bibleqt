c64745480d5645ae53a01f71e5c20810268e5ce3
sergio <serhiy_kovalchuk@race.es>
2023-09-21 19:07:05 +0200

    2023-09-21 19:06 - arreglado fonts greek y hebrew - 1

--- bible_app.php
+++ bible_app.php
@@ -9,10 +9,7 @@ header('Content-type: text/html; charset=utf-8');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BQ test</title>
     <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet'>
-     
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+Hebrew&display=swap" rel="stylesheet">
+
     
 
     <link rel="stylesheet" href="bible_app.css">
