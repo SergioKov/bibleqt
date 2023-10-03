0fd3b0a47bfac7d74f1b99f1ce91fb1b5f47e515
sergio <serhiy_kovalchuk@race.es>
2023-10-03 20:23:58 +0200

    2023-10-03 20:11 - añado favicon - 1

--- bible_app.php
+++ bible_app.php
@@ -8,6 +8,7 @@ header('Content-type: text/html; charset=utf-8');
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>BQ test</title>
+    <link rel="icon" type="image/png" href="./images/bq.png">
     <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet'>    
 
     <link rel="stylesheet" href="bible_app.css">
