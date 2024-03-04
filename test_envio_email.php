9c45fe487a8e8bdf309f1ae79552e7e6ab764a3f
sergio <serhiy_kovalchuk@race.es>
2024-03-04 13:02:02 +0100

    2024-04-04 guardo test_envio_email.php - 1

--- test_envio_email.php
+++ test_envio_email.php
@@ -1,23 +1,31 @@
 <?php
-$para = "sergiokovalchuk@gmail.com";
-$asunto = "Prueba de correo electrónico";
-$mensaje = "Este es un mensaje de prueba.";
-$from_email = "sergiokovalchuk@gmail.com";
-$reply_to_email = "sergiokovalchuk@gmail.com";
+
+$para = "sergbas2001@gmail.com,sergiokovalchuk@gmail.com";
+$asunto = "Prueba de correo electrónico 4";
+$mensaje = "Este es un <b>mensaje de prueba en negrita</b>. <h3>envio</h3> a 2 emails separados por coma. Prueba de correo electrónico 444 desde el fichero: <b>https://bibleqt.es/__test_envio_email.php</b>";
+
+
+echo "<p>$ para: $para";
+echo "<p>$ asunto: $asunto";
+echo "<p>$ mensaje: $mensaje";
+
 
 // Для отправки HTML-письма должен быть установлен заголовок Content-type
 $headers  = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type: text/plain; charset=\"utf-8\"" . "\r\n";
-$headers .= "From: mitest - YOOOO <" . $from_email . ">" . "\r\n"; 
-$headers .= "Reply-To: Sergio <" . $reply_to_email . ">" . "\r\n"; 
+$headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
+$headers .= "From: mitest - YO <contact@serg.es>" . "\r\n"; 
+$headers .= "Reply-To: Sergio <sergiokovalchuk@gmail.com>" . "\r\n"; 
+
+
+echo "<pre>$ headers:<br> $headers </pre>";
 
 try {
     $result = mail($para, $asunto, $mensaje, $headers);
 
     if ($result) {
-        echo "1. El correo electrónico se envió correctamente.";
+        echo "<p>(if). El correo electrónico se envió correctamente.";
     } else {
-        echo "2. Error al enviar el correo electrónico.";
+        echo "<p>(else). Error al enviar el correo electrónico.";
     }
 
 } catch (\Throwable $th) {
