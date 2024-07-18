6704a272df59792d8ca17bd4ec2f0fb3720780bb
sergio <serhiy_kovalchuk@race.es>
2024-07-18 14:06:44 +0200

    develop2 - 2024-07-18 - arreglo scrool in row. comento console.log() - 2

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>BQ test</title>
+    <title>BQ</title>
     <link rel="icon" type="image/png" href="./images/bq.png">
     <link href='https://fonts.googleapis.com/css?family=Muli' rel='stylesheet'>    
 
