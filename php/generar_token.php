155337a1a986d05a95aa43b5c8d3caf898f01856
sergio <serhiy_kovalchuk@race.es>
2024-08-28 15:19:23 +0200

    dev2 - edito envio email - 2

--- php/generar_token.php
+++ php/generar_token.php
@@ -51,13 +51,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         $subject = "Restablecer Contraseña";
         $message = "Haga clic en el siguiente enlace para restablecer su contraseña: $resetLink";
 
-        $message_html = `
+        $message_html = '
+        <div marginheight="0" marginwidth="0" style="width:100%!important;margin:0;padding:0;background: red;">    
             <table border="0" cellpadding="0" cellspacing="0" role="presentation" width="100%">
                 <tbody>
                 <tr>
                     <td align="left" style="font-size:0px;padding:32px 44px;word-break:break-word">
                         <div style="font-family:Ubuntu,Helvetica,Arial,sans-serif;font-size:16px;line-height:1.4;text-align:left;color:#253238">
-                            Hola, <a href="mailto:$email" target="_blank">$email</a>:
+                            Hola, <a href="mailto:' . $email . '" target="_blank">' . $email . '</a>:
                         </div>
                     </td>
                 </tr>
@@ -78,7 +79,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <tr>
                     <td align="center" style="font-size:0px;padding:32px 44px;padding-bottom:10px;word-break:break-word">
                         <a 
-                        href="$resetLink" 
+                        href="' . $resetLink . '" 
                         style="background:#13c1ac;color:#ffffff;font-family:Raleway,Arial;font-size:16px;font-weight:normal;line-height:120%;Margin:0;text-decoration:none;text-transform:none;border-radius:40px;padding:10px 25px" 
                         target="_blank"
                         >
@@ -95,7 +96,8 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 </tr>
                 </tbody>
             </table>
-        `;
+        </div>
+        ';
         
         //$from_email = "sergiokovalchuk@gmail.com";
         //$reply_to_email = "sergiokovalchuk@gmail.com";
@@ -112,7 +114,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
         $headers  = "MIME-Version: 1.0" . "\r\n";
         $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
-        $headers .= "From: bq mitest - YO <contact@serg.es>" . "\r\n"; 
+        $headers .= "From: Bibleqt - YO <contact@serg.es>" . "\r\n"; 
         $headers .= "Reply-To: Sergio <sergiokovalchuk@gmail.com>" . "\r\n"; 
 
 
