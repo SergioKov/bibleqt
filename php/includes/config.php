03c25d8dd2395149a54c6bf12b6c76b55fbb2f34
sergio <serhiy_kovalchuk@race.es>
2024-09-10 10:31:52 +0200

    dev2 - edito config.php - 2

--- php/includes/config.php
+++ php/includes/config.php
@@ -1,7 +1,10 @@
 <?php
 
-//$arr_metodos = ['POST'];//en PROD siempre!
-$arr_metodos = ['POST', 'GET'];//para hacer test... 
+if($_SERVER['HTTP_HOST'] == 'bibleqt.es'){//HOSTALIA
+    $arr_metodos = ['POST'];//en PROD siempre!   
+}else{//LOCALHOST
+    $arr_metodos = ['POST', 'GET'];//para hacer test...  
+}
 
-
+//$arr_metodos = ['POST', 'GET'];//descomentar para hacer test y comentar en PROD... 
 ?>
